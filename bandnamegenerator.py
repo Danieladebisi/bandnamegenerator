d83241c13a689b98a2132165cd84ad785392866b
Adebisi Daniel <danidannyco@gmail.com>
2022-08-31 23:06:33 +0100

    first commit

---
+++ bandnamegenerator.py
@@ -0,0 +1,9 @@
+from operator import add
+from unicodedata import name
+
+
+ # Project band name generator 
+print('Welcome to the band name generator')
+first_info = input('What is your name?\n')
+second_info = input('What is your dog\'s name?\n')
+print('You can name your band - ', first_info + '' + second_info)
